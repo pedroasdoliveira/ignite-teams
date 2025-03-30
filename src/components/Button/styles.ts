11b2b3225e234568f5639f19fbb7c37046075094
Pedro Oliveira <pedroaolive@gmail.com>
2025-03-30 17:16:25 -0300

    style: add disabled button style

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,11 +1,12 @@
 import { TouchableOpacity } from "react-native";
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 import { DefaultTheme } from "styled-components/native";
 
 export type ButtonTypeStyleProps = "PRIMARY" | "SECONDARY";
 
 type Props = {
   type: ButtonTypeStyleProps;
+  disabled: boolean;
 };
 
 export const Container = styled(TouchableOpacity)<Props>`
@@ -14,18 +15,17 @@ export const Container = styled(TouchableOpacity)<Props>`
   min-height: 56px;
   max-height: 56px;
 
-  background-color: ${({
-    theme,
-    props,
-  }: {
-    theme: DefaultTheme;
-    props: Props;
-  }) =>
-    props.type === "PRIMARY" ? theme.COLORS.GREEN_700 : theme.COLORS.RED_DARK};
+  background-color: ${({ theme, type }: { theme: DefaultTheme; type: ButtonTypeStyleProps }) =>
+    type === "PRIMARY" ? theme.COLORS.GREEN_700 : theme.COLORS.RED_DARK};
+
   border-radius: 6px;
 
   justify-content: center;
   align-items: center;
+
+  ${({disabled}: {disabled: boolean}) => disabled && css`
+    background-color: ${({theme}: {theme: DefaultTheme}) => theme.COLORS.GRAY_400};
+  `}
 `;
 
 export const Title = styled.Text`
