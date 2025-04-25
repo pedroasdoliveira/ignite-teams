7546a7167c3b70f00e28f7c1482a01d24b4313be
Pedro Oliveira <pedroaolive@gmail.com>
2025-04-25 17:36:52 -0300

    feat: add function to add a new group

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,19 +1,40 @@
 import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
+import { Alert } from "react-native";
+
 import { Container, Content, Icon } from "./styles";
 
 import Header from "@components/Header";
 import Highlight from "@components/Highlight";
 import Button from "@components/Button";
 import Input from "@components/Input";
+import { groupCreate } from "@storage/group/groupCreate";
+import { AppError } from "@utils/AppError";
 
 const NewGroup = () => {
   const navigation = useNavigation();
 
-  const [group, setGroup] = useState('');
+  const [group, setGroup] = useState("");
 
-  const handleNew = () => {
-    navigation.navigate('players', { group });
+  const handleNew = async () => {
+    try {
+      if (group.trim().length === 0) {
+        return Alert.alert("Novo Grupo", "Informe o nome da turma.");
+      }
+
+      await groupCreate(group);
+      navigation.navigate("players", { group });
+    } catch (error) {
+      if (error instanceof AppError) {
+        return Alert.alert("Novo Grupo", error.message);
+      } else {
+        console.error(error);
+        return Alert.alert(
+          "Novo Grupo",
+          "Não foi possível criar um novo grupo."
+        );
+      }
+    }
   };
 
   return (
